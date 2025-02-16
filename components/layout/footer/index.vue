1533716c4e25cbb615b8915697d99fb154cf3c7c
treetospon <treetospon@thelivingos.com>
2025-02-16 16:20:46 +0700

    refector

---
+++ components/layout/footer/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="mt-[7rem] pb-[2rem] bottom-0 border-t-1 border-t-gray">
+    <div class="flex justify-center">
+      <div class="w-[64rem] py-[2rem]">
+        <div class="flex justify-between mb-[3rem]">
+          <div>
+            <div class="flex">
+              <Logo title="Elias" />
+              <p class="font-mitr-light text-[1rem] text-gray ml-[0.5rem]">
+                elias@elias-dev.ml
+              </p>
+            </div>
+            <p class="font-mitr-light text-[1rem] text-gray mt-[0.5rem]">
+              Web designer and front-end developer
+            </p>
+            <a
+              href="https://www.figma.com/community/file/1164933568884615740"
+              target="_blank"
+              ><p
+                class="font-mitr-light text-[0.875rem] text-gray mt-[0.5rem] hover:text-white"
+              >
+                Design Credit ~ Elias from Figma community
+              </p></a
+            >
+          </div>
+          <div class="justify-items-center">
+            <p class="font-mitr text-[1.5rem] text-gray mt-[0.5rem]">Media</p>
+            <div class="flex justify-center py-[1rem]">
+              <AppSvg name="/assets/svg/contacts/Github.svg" class="px-[0.25rem]" />
+              <AppSvg name="/assets/svg/contacts/Linkedin.svg" class="px-[0.25rem]" />
+            </div>
+          </div>
+        </div>
+        <p class="flex justify-center font-mitr-light text-[1rem] text-gray">
+          © Copyright 2022. Made by Elias
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ components/layout/header/menu.vue
@@ -0,0 +1,11 @@
+<template>
+  <p class="font-mitr-light text-[1rem] text-gray ml-[2rem] hover:font-mitr hover:text-white">
+    <span class="text-primary">#</span>{{ menu }}
+  </p>
+</template>
+
+<script lang="ts" setup>
+const { menu } = defineProps({
+  menu: { type: String, required: true },
+});
+</script>
